3fb8745cae0ce854d9599ce6a3460013bfb4d045
EndriwMichel <endriwmichel@gmail.com>
2016-06-25 01:42:47 -0300

    Commit tela de cadastro
    uma pequena comitada da tela de cadastro

--- Map_v2.1/app/src/main/java/com/example/endriw/map_v21/CadDog.java
+++ Map_v2.1/app/src/main/java/com/example/endriw/map_v21/CadDog.java
@@ -1,14 +1,20 @@
 package com.example.endriw.map_v21;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
 
 public class CadDog extends AppCompatActivity {
 
     public Toolbar toolbar;
+    private String array_spinner[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,19 @@ public class CadDog extends AppCompatActivity {
         setContentView(R.layout.activity_cad_dog);
        // toolbar = (Toolbar) findViewById(R.id.dog_toolbar);
        // setSupportActionBar(toolbar);
+
+        array_spinner=new String[5];
+        array_spinner[0]="option 1";
+        array_spinner[1]="option 2";
+        array_spinner[2]="option 3";
+        array_spinner[3]="option 4";
+        array_spinner[4]="option 5";
+
+        Spinner s = (Spinner) findViewById(R.id.dogSpi);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, array_spinner);
+        s.setAdapter(adapter);
+
 
     }
 
@@ -42,6 +61,7 @@ public class CadDog extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
 
 }
