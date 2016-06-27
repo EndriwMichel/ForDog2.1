a14669124de4bbdf0cbd36b1bc228e6799ab5da6
EndriwMichel <endriwmichel@gmail.com>
2016-06-27 14:27:11 -0300

    tela cadastro calendário
    consertando o calendário da tela de cadastro

--- Map_v2.1/app/src/main/java/com/example/endriw/map_v21/CadDog.java
+++ Map_v2.1/app/src/main/java/com/example/endriw/map_v21/CadDog.java
@@ -1,29 +1,46 @@
 package com.example.endriw.map_v21;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 public class CadDog extends AppCompatActivity {
 
     public Toolbar toolbar;
     private String array_spinner[];
+    private CalendarView calendar;
+    private PopupWindow pup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_dog);
+        calendar = (CalendarView)findViewById(R.id.calendarDog);
+
        // toolbar = (Toolbar) findViewById(R.id.dog_toolbar);
        // setSupportActionBar(toolbar);
 
-        array_spinner=new String[5];
+     /*   array_spinner=new String[5];
         array_spinner[0]="option 1";
         array_spinner[1]="option 2";
         array_spinner[2]="option 3";
@@ -34,7 +51,7 @@ public class CadDog extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_item, array_spinner);
         s.setAdapter(adapter);
-
+*/
 
     }
 
@@ -63,5 +80,55 @@ public class CadDog extends AppCompatActivity {
     }
 
 
+    public void CallCalendar(View view) {
 
+        System.out.println("oi");
+
+        PopupWindow popup = new PopupWindow(CadDog.this);
+        View layout = getLayoutInflater().inflate(R.layout.calendar_lay, null);
+        popup.setContentView(layout);
+
+        popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+        popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+
+        popup.setOutsideTouchable(true);
+        popup.setFocusable(true);
+
+        popup.setBackgroundDrawable(new BitmapDrawable());
+        popup.showAsDropDown(view);
+     /*   LayoutInflater inflater = (LayoutInflater) CadDog.this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View layout = inflater.inflate(R.layout.calendar_lay,
+                (ViewGroup) findViewById(R.id.calendarView));
+
+        pup = new PopupWindow(layout, 1200, 1500, true);
+
+        calendar = (CalendarView) layout.findViewById(R.id.calendarView);
+
+        calendar.setShowWeekNumber(false);
+
+        calendar.setFirstDayOfWeek(2);
+
+        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.lightGreen));
+
+        //sets the color for the dates of an unfocused month.
+        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.colorPrimary));
+
+        //sets the color for the separator line between weeks.
+        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.colorAccent));
+
+        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
+        calendar.setSelectedDateVerticalBar(R.color.colorPrimaryDark);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                System.out.println("oiiii");
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+*/
+
+    }
 }
