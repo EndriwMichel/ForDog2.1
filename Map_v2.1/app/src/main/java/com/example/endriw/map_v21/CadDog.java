74231954d9f6a706aeaccfbbef45acf312c1263b
EndriwMichel <endriwmichel@gmail.com>
2016-06-29 01:50:11 -0300

    alterações 29-06
    alterações no calendário tela de cadastro

--- Map_v2.1/app/src/main/java/com/example/endriw/map_v21/CadDog.java
+++ Map_v2.1/app/src/main/java/com/example/endriw/map_v21/CadDog.java
@@ -1,134 +1,53 @@
 package com.example.endriw.map_v21;
 
+
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 
-public class CadDog extends AppCompatActivity {
+public class CadDog extends AppCompatActivity  {
 
     public Toolbar toolbar;
     private String array_spinner[];
-    private CalendarView calendar;
     private PopupWindow pup;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_dog);
-        calendar = (CalendarView)findViewById(R.id.calendarDog);
 
-       // toolbar = (Toolbar) findViewById(R.id.dog_toolbar);
-       // setSupportActionBar(toolbar);
 
-     /*   array_spinner=new String[5];
-        array_spinner[0]="option 1";
-        array_spinner[1]="option 2";
-        array_spinner[2]="option 3";
-        array_spinner[3]="option 4";
-        array_spinner[4]="option 5";
-
-        Spinner s = (Spinner) findViewById(R.id.dogSpi);
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_item, array_spinner);
-        s.setAdapter(adapter);
-*/
-
-    }
-
-    public void setSupportActionBar(Toolbar toolbar) {
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.caddog_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
     public void CallCalendar(View view) {
+        DialogFragment dialogfragment = new DogCalendar();
 
-        System.out.println("oi");
+        dialogfragment.show(getFragmentManager(), "Dog Calendar !!");
+    }
 
-        PopupWindow popup = new PopupWindow(CadDog.this);
-        View layout = getLayoutInflater().inflate(R.layout.calendar_lay, null);
-        popup.setContentView(layout);
 
-        popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
-        popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+    public void ClickSalvar(View view) {
+        Toast.makeText(getApplicationContext(), "oi", Toast.LENGTH_SHORT).show();
+    }
 
-        popup.setOutsideTouchable(true);
-        popup.setFocusable(true);
+    public void ClickCancelar(View view) {
 
-        popup.setBackgroundDrawable(new BitmapDrawable());
-        popup.showAsDropDown(view);
-     /*   LayoutInflater inflater = (LayoutInflater) CadDog.this
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        View layout = inflater.inflate(R.layout.calendar_lay,
-                (ViewGroup) findViewById(R.id.calendarView));
-
-        pup = new PopupWindow(layout, 1200, 1500, true);
-
-        calendar = (CalendarView) layout.findViewById(R.id.calendarView);
-
-        calendar.setShowWeekNumber(false);
-
-        calendar.setFirstDayOfWeek(2);
-
-        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.lightGreen));
-
-        //sets the color for the dates of an unfocused month.
-        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.colorPrimary));
-
-        //sets the color for the separator line between weeks.
-        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.colorAccent));
-
-        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
-        calendar.setSelectedDateVerticalBar(R.color.colorPrimaryDark);
-
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                System.out.println("oiiii");
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
-            }
-        });
-*/
-
+        this.finish();
     }
 }
---
+++ Map_v2.1/app/src/main/java/com/example/endriw/map_v21/DogCalendar.java
@@ -0,0 +1,40 @@
+package com.example.endriw.map_v21;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+/**
+ * Created by Endriw on 28/06/2016.
+ */
+public class DogCalendar extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(),
+                AlertDialog.THEME_DEVICE_DEFAULT_DARK ,this,year,month,day);
+
+        return datepickerdialog;
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+        EditText data = (EditText)getActivity().findViewById(R.id.editText4);
+
+        data.setText(dayOfMonth + " / " + (monthOfYear+1) + " / " + year);
+
+    }
+}
